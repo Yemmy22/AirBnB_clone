803d839fbdd6fbe145758413fc2be4088232fcf1
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-03-10 08:51:09 +0000

    Create a programe that writes a string

---
+++ pycodestyle_okay.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+A pycodestyle compliant module'
+'''
+
+
+def pycodestyle_compliant():
+    '''
+    A function that prints a string.
+    '''
+    print("This code is pycodestyle compliant.\n")
+
+
+if __name__ == "__main__":
+    pycodestyle_compliant()
