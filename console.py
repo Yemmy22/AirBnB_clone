aea2ff3518b52e2cea131597c5dfa51d24e083dd
Yemmy22 <opeyemiadedigba20@gmail.com>
2024-03-24 17:56:36 +0000

    Fix error output when an non-existing class is passed to the full range of argument suffix of the show, update and destroy commands in the console.

--- console.py
+++ console.py
@@ -15,7 +15,7 @@ class HBNBCommand(cmd.Cmd):
     '''
 
     prompt = '(hbnb) '
-    class_list = ["BaseModel", "User", "Fake"]
+    class_list = ["BaseModel", "User"]
 
     def do_EOF(self, line):
         return True
@@ -33,7 +33,9 @@ class HBNBCommand(cmd.Cmd):
         return
 
     def do_create(self, line):
-        """Create an instance of a specified class"""
+        """
+        Create an instance of a specified class
+        """
         if line:
             if line in self.class_list:
                 new_obj = eval(line)()
@@ -45,40 +47,45 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def do_show(self, line):
-        """Display the string representation of an instance"""
+        """
+        Display the string representation of an instance
+        """
         if line:
             args = line.split()
             if len(args) <= 2:
-                if len(args) == 1:
-                    if args[0] not in self.class_list:
-                        print("** class doesn't exist **")
-                    else:
-                        print("** instance id missing **")
+                if args[0] not in self.class_list:
+                    print("** class doesn't exist **")
                 else:
-                    all_obj = storage.all()
-                    for obj_key in all_obj:
-                        key = "{}.{}".format(args[0], args[1])
-                        if key == obj_key:
-                            print(all_obj[obj_key])
-                            return
-                    print("** no instance found **")
+                    if len(args) < 2:
+                        print("** instance id missing **")
+                    else:
+                        all_obj = storage.all()
+                        for obj_key in all_obj.keys():
+                            key = "{}.{}".format(args[0], args[1])
+                            if key == obj_key:
+                                print(all_obj[obj_key])
+                                return
+                        print("** no instance found **")
         else:
             print("** class name missing **")
 
     def do_destroy(self, line):
-        """Delete an instance based on the class name and id"""
+        """
+        Delete an instance based on the
+        class name and id
+        """
         if line:
             args = line.split()
-            if len(args) <= 2:
-                if len(args) == 1:
-                    if args[0] not in self.class_list:
-                        print("** class doesn't exist **")
-                    else:
-                        print("** instance id missing **")
-                else:
+            if args[0] not in self.class_list:
+                print("** class doesn't exist **")
+            else:
+                if len(args) < 2:
+                    print("** instance id missing **")
+                elif len(args) == 2:
                     all_obj = storage.all()
-                    for obj_key, obj in storage.all().items():
-                        key = "{}.{}".format(args[0], args[1])
+                    for obj_key in all_obj:
+                        key = "{}.{}".\
+                            format(args[0], args[1])
                         if key == obj_key:
                             del(all_obj[key])
                             storage.save()
@@ -88,7 +95,9 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
 
     def do_all(self, line):
-        """Display string representations of all instances"""
+        """
+        Display string representations of all instances
+        """
         obj_list = []
         if line:
             for obj in storage.all().values():
@@ -103,39 +112,42 @@ class HBNBCommand(cmd.Cmd):
         print(obj_list)
 
     def do_update(self, line):
-        """Update an instance attribute"""
+        """
+        Update an instance attribute
+        """
         if line:
             arg = line.split(' ')
-            if len(arg) == 1:
-                if arg[0] not in self.class_list:
-                    print("** class doesn't exist **")
-                else:
-                    print("** instance id missing **")
-            elif len(arg) == 2:
-                obj_copy = False
-                for obj in storage.all().values():
-                    if arg[1] == obj.id:
-                        obj_copy = True
-                if obj_copy is True:
-                    print("** attribute name missing **")
-                else:
-                    print("** no instance found **")
-            elif len(arg) == 3:
-                print("** value missing **")
+            if arg[0] not in self.class_list:
+                print("** class doesn't exist **")
             else:
-                const_attr = ["id", "created_at", "updated_at"]
-                if arg[2] not in const_attr:
-                    all_obj = storage.all()
-                    for obj_key, obj in all_obj.items():
-                        key = "{}.{}".format(arg[0], arg[1])
-                        if key == obj_key:
-                            attr = arg[2]
-                            attr_type = type(arg[2])
-                            attr_value = arg[3].strip('"').strip("'")
-                            setattr(obj, attr, attr_type(attr_value))
-                            obj.save()
-                            return
-                    print("** no instance found **")
+                if len(arg) < 2:
+                    print("** instance id missing **")
+                else:
+                    obj_copy = False
+                    for obj in storage.all().values():
+                        if arg[1] == obj.id:
+                            obj_copy = True
+                    if not obj_copy:
+                        print("** no instance found **")
+                    else:
+                        if len(arg) < 3:
+                            print("** attribute name missing **")
+                        elif len(arg) < 4:
+                            print("** value missing **")
+                        else:
+                            const_attr = ["id", "created_at", "updated_at"]
+                            if arg[2] not in const_attr:
+                                all_obj = storage.all()
+                                for obj_key, obj in all_obj.items():
+                                    key = "{}.{}".format(arg[0], arg[1])
+                                    if key == obj_key:
+                                        attr = arg[2]
+                                        attr_type = type(arg[2])
+                                        attr_val = arg[3].strip('"').strip("'")
+                                        setattr(obj, attr, attr_type(attr_val))
+                                        obj.save()
+                                        return
+                                    print("** no instance found **")
         else:
             print("** class name missing **")
 
